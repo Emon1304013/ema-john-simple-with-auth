5f5ebc55f58d0c5e191b4fbde8b730e913423dc8
KamalEmon <u1304013@student.cuet.ac.bd>
2022-10-18 07:35:22 +0600

    User signin functionality using email and password created. Firebase

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../contexts/UserContext';
 import './Login.css'
 
 const Login = () => {
+    const {loginUser} = useContext(AuthContext)
 
-    const handleLogin = () => {
+    const handleLogin = (event) => {
+        event.preventDefault()
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email,password);
         
-    }
+        loginUser(email,password)
+        .then(result => {
+            const user = result.user;
+            console.log(user);
+            alert("User logged in successfull")
+        })
+        .catch(error => {
+            alert("Login credentials wrong")
+        })
+
+        form.reset();
+
+    }   
 
 
 
@@ -19,15 +38,15 @@ const Login = () => {
                     <form onSubmit={handleLogin}>
                         <div className="form-control">
                             <label htmlFor="email">Email</label>
-                            <input type="email" name="email" id="" placeholder='Email' required />
+                            <input type="email" name="email" placeholder='Email' required />
                         </div>
 
                         <div className="form-control">
                             <label htmlFor="password">Password</label>
-                            <input type="password" name="password" id="" placeholder='Password' required />
+                            <input type="password" name="password" placeholder='Password' required />
                         </div>
 
-                        <input className='btn-sign-up' type="submit" value="Sign Up" />
+                        <input className='btn-sign-up' type="submit" value="Login" />
                         <p className='already-account'><small>New to Ema-John? <Link to='/signup'>Create New Account</Link></small></p>
                     </form>
                 </div>
--- src/components/contexts/UserContext.js
+++ src/components/contexts/UserContext.js
@@ -1,5 +1,5 @@
 import React, { createContext, useState } from 'react';
-import {createUserWithEmailAndPassword, getAuth} from 'firebase/auth'
+import {createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signOut} from 'firebase/auth'
 import app from '../../firebase/firebase.config';
 
 export const AuthContext = createContext({});
@@ -9,10 +9,19 @@ const auth = getAuth(app)
 const UserContext = ({children}) => {
 
     const [user,setUser] = useState(null);
+
     const createUser = (email,password) => {
         return createUserWithEmailAndPassword(auth, email, password);
     }
-    const authInfo = {createUser}
+
+    const loginUser = (email,password) => {
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    const logOutUser = () => {
+        return signOut(auth);
+    }
+    const authInfo = {createUser,loginUser,logOutUser}
 
     return (
         <div>
